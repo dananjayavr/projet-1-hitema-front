a49515c80b44846ad263ecbf458e9cfccb742ebb
Dan Ramanayake <dananjaya86@yahoo.com>
2019-04-13 22:30:02 +0200

    design header et footer

--- inc/header.php
+++ inc/header.php
@@ -7,9 +7,48 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/main.css">
     <title>Cooking!</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="./assets/images/logo-cooking.png" alt="Cooking Logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Se connecter<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Créer un compte</a>
+            </li>
+            <li class="nav-item dropdown active">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Plus
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Recettes</a>
+                    <a class="dropdown-item" href="#">Menus</a>
+                    <a class="dropdown-item" href="#">Minceur</a>
+                    <a class="dropdown-item" href="#">Atelier</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Contact</a>
+                </div>
+            </li>
+        </ul>
+        <span class="navbar-text">
+            <i class="fab fa-facebook-f p-1"></i>
+            <i class="fab fa-twitter p-1"></i>
+            <i class="fab fa-google-plus-g p-1"></i>
+            <i class="fab fa-youtube p-1"></i>
+        </span>
+    </div>
+</nav>
 <!-- Content Below -->
--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 include './inc/header.php';
-echo "Hello, World!";
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
+
+<?php
 include './inc/footer.php';
 ?>
