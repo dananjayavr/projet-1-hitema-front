8fd76f80586642cf6fd84c170e57f7e0bff972e7
Dan Ramanayake <dananjaya86@yahoo.com>
2019-04-24 19:30:00 +0200

    fixed footer 404.php

--- 404.php
+++ 404.php
@@ -2,14 +2,29 @@
 include 'inc/header.php';
 ?>
 <div class="container">
-    <div class="row">
-        <div class="col-6 p-5">
+    <div class="row mx-auto">
+        <div class="col-12 text-center">
             <h1>Page non trouvée</h1>
             <a href="index.php">Retour à l'accueil</a>
         </div>
-        <div class="col-6 p-5">
-            <img src="assets/photos/backgrounds/bg-404.png" alt="spilled milk" class="img-fluid">
-        </div>
     </div>
 </div>
-<?php include 'inc/footer.php'; ?>
+
+<nav class="navbar navbar-light bg-light mt-5 fixed-bottom" id="footer">
+    <span class="navbar-text mx-auto">
+        <a href="#">Recettes</a> | <a href="#">Menus</a> | <a href="#">Desserts</a> | <a href="#">Minceur</a> | <a
+            href="#">Atelier</a> | <a href="#">Contact</a>
+        <br>
+        <div class="socialFooter text-center">
+            <i class="fab fa-facebook-f p-2"></i>
+            <i class="fab fa-twitter p-2"></i>
+            <i class="fab fa-google-plus-g p-2"></i>
+            <i class="fab fa-youtube p-2"></i>
+        </div>
+    </span>
+</nav>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
