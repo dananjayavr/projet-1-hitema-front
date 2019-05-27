664b95655f07661cfaf10ee933857159bc7fe9dd
Dan Ramanayake <dananjaya86@yahoo.com>
2019-05-27 19:18:54 +0200

    change photo recipe

--- recette-detail.php
+++ recette-detail.php
@@ -26,6 +26,57 @@ if (!$recette) {
         <div class="col-sm-6 pb-5">
             <img src="./assets/photos/recettes/<?php echo $recette->img;?>" class="rounded img-fluid" alt="<?php echo substr($recette->img,0, strpos($recette->img,'.'));?>" id="imageRecette">
         </div>
+        <?php if (isset($_SESSION['login']) and $_SESSION['idMembre'] == $recette->idMembre) { ?>
+            <br>
+            <div id="alertBox"></div>
+            <br>
+            <form action="recette-detail.php?idr=<?=$id_recette;?>" method="post" enctype="multipart/form-data">
+                <div class="custom-file row mx-auto">
+                    <div class="col-xs-12 col-lg-6">
+                        <small class="custom-file-label for="image">Changer Image</small>
+                        <input type="file" class="custom-file-input form-control input-sm" id="image" name="image" accept="image/png, image/jpeg" onchange="this.form.submit()">
+                    </div>
+                </div>
+            </form>
+            <script>
+                $('#image').on('change',() => {
+                    let fileName = $('#image').val().replace('C:\\fakepath\\', " ");
+                    $('#image').next('.custom-file-label').html(fileName);
+                });
+            </script>
+        <?php }?>
+        <?php
+        if(!empty($_FILES['image'])) {
+            $image = $_FILES['image']['name'];
+            $imageName = md5(uniqid($image)).".".pathinfo($_FILES['image']['name'],PATHINFO_EXTENSION);
+            $path = "assets/photos/recettes/";
+            //$path = $path . basename($_FILES['image']['name']);
+            $path = $path.$imageName;
+            if ($_FILES['image']['size'] > 200000) { ?>
+                <script>
+                    $(document).ready(() => {
+                        $('#alertBox').append("<div class=\"alert alert-danger\" role=\"alert\">\n" +
+                            "  Votre image est trop grand. Veuillez choisir une autre image.\n" +
+                            "</div>");
+                    });
+                </script>
+            <?php } else {
+                if (move_uploaded_file($_FILES['image']['tmp_name'], $path)) {
+                    //crop_image($_FILES['image']['tmp_name'],pathinfo($_FILES['image']['name'],PATHINFO_EXTENSION));
+                    $uploadOK = true;
+                    $query_p = "UPDATE recettes SET img=\"$imageName\" WHERE idRecette=$id_recette";
+                    $pdo->query($query_p);
+                } else {
+                    echo <<< _END
+                    <script>
+                    $(document).ready(() => {
+                        $('#alertBox').append("<div class='alert alert-danger' role='alert'>Un erreur a été détécté. Veuillez vérifier votre image.</div>");
+                    });
+                    </script>
+_END;
+                }
+            }
+        } ?>
     </div>
     <div class="row mx-auto">
         <div class="col-sm-6 col-md-12 p-2">
